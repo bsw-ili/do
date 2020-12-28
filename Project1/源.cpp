afa2e9c8c6adae9da6c986ff2ed00a7f409f8b3e
bsw-ili <jianhangy569@gmail.com>
2020-12-28 20:13:18 +0800

    增加了一些功能

--- Project1/源.cpp
+++ Project1/源.cpp
@@ -9,11 +9,12 @@ struct student
     char sex;
     int score[4];
     int he;
+    int paiming;
 };
 void print();
 int main()
 {
-    int i = 0,j,c;
+    int i = 0,j,c,t;
     int sum[4] = { '\0' };
     double ave[4] = { '\0' };
     int a;
@@ -101,7 +102,71 @@ int main()
             }
             printf("\n");
         }
-        printf("请输入1 ，2 ，3, 4\n");
+        if (a == 5)
+        {
+            for (int m = 0; m < i; m++)
+            {
+                stu[m].paiming = m + 1;
+                stu[m].he = 0;
+                for (int j = 0; j < 4; j++)
+                {
+                    stu[m].he += stu[m].score[j];
+                }
+            }
+            for (int m = 0; m < i - 1; m++)
+            {
+                for (int n = m + 1; n < i; n++)
+                {
+                    if (b[m].he < b[n].he)
+                    {
+                        temp.paiming = stu[m].paiming;
+                        stu[m].paiming = stu[n].paiming;
+                        stu[n].paiming = temp.paiming;
+                        temp = b[m];
+                        b[m] = b[n];
+                        b[n] = temp;
+                    }
+                }
+            }
+            for (int m = 0; m < i; m++)
+            {
+                printf("排名：%d\t", stu[m].paiming);
+                printf("%d\t", stu[m].he);
+                printf("%d\t", stu[m].ID);
+                printf("%s\t", stu[m].name);
+                printf("%c\t", stu[m].sex);
+                for (int j = 0; j < 4; j++)
+                {
+                    printf("%d\t", stu[m].score[j]);
+                }
+                printf("\n");
+            }
+            printf("\n");
+        }
+        if (a == 6)
+        {
+            printf("请先输入a=5");
+            printf("请输入你要查找的学号：");
+            scanf_s("%d", &t);
+            for (j = 0; j < i; j++)
+            {
+                if (stu[j].ID == t)
+                {
+                    printf("排名：%d\t", stu[j].paiming);
+                    printf("%d\t", stu[j].he);
+                    printf("%d\t", stu[j].ID);
+                    printf("%s\t", stu[j].name);
+                    printf("%c\t", stu[j].sex);
+                    for (int m = 0; m < 4; m++)
+                    {
+                        printf("%d\t", stu[j].score[m]);
+                    }
+                    printf("\n");
+                    break;
+                }
+            }
+        }
+        printf("请输入1 ，2 ，3, 4, 5 ,6\n");
     }
     return 0;
 }
@@ -110,9 +175,10 @@ void print()
     printf("学生管理系统\n");
     printf("1.增加一个人的信息\n");
     printf("2.删除一个人的信息\n");
-    printf("3.展示信息\n");
+    printf("3.按学号展示信息\n");
     printf("4.各科平均值\n");
-    printf("请输入1，2，3, 4\n");
-
+    printf("5.按成绩展示个人信息\n");
+    printf("6.按学号查找个人信息\n");
+    printf("请输入1，2，3, 4 ,5 ,6\n");
 }
    
